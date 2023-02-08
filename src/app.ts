c8597b0116bcb35b3a1b87e9aca08fb4d1cd7063
Erik Frisk <erik@frisk.cc>
2023-02-09 00:21:12 +0800

    Support resize

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,23 @@
 // https://coolors.co/130325-be4b88-00a1d6-004d66-d7c27d
 
-import {
-  CanvasSpace,
-  Pt,
-  Create,
-  Group,
-  Tempo,
-  Mat,
-  Circle,
-  Line,
-  Num,
-} from 'pts';
+import { CanvasSpace, Pt, Group, Line, Bound } from 'pts';
 import seedrandom from 'seedrandom';
 import { transparentize } from 'color2k';
 
 // Todo:
 // [x] Make circles wiggle
 // [x] Possibly add fraying to the outer edges of circles (UPDATE: NO!)
+// [x] Fix resize
 // [ ] Add query string param for turning off animation
 // [ ] Refactor features and make simple iterations more rare
 // [ ] Add fxhash seed
-// [ ] Fix reload
 // [ ] Center canvas in window
+// [ ] Fade in on load
+// [ ] Refactor moveSpeed (two rotation speeds in different formats)
+// [ ] Refactor circle/portal names
+// [ ] Give it bound height on wide screens
+// [ ] Give lines a minimum thickness when canvas is small
+// [ ] If animation is turned off, make sure it doesn't animate when resized
 
 const SEED = (window as any).fxhash;
 // const SEED = 'oozo2eYjpL8wbrVtHbfL9N8CEUaENiMqzLU6voWyCb9nVDK4Bru';
@@ -45,34 +41,58 @@ const col = [
   COLORS[getRandomIntBetween(1, 4)],
   COLORS[getRandomIntBetween(1, 4)],
 ].sort();
+const N_CIRCLES = getRandomIntBetween(3, 6);
 
-const getRandomLinesCircle = (space) => {
-  const pCenter = new Pt(
-    space.width * gaussianRng(),
-    space.height * gaussianRng()
-  );
-  const radius = space.width / getRandomBetween(0.8, 5);
-  const N_LINES = getRandomIntBetween(100, 200);
-  const rotation = getRandomBetween(0.2, 0.27);
-  const rotationDirection = [-1, 1][getRandomIntBetween(0, 1)];
+const getRandomPortalParams = () => {
+  const nLines = getRandomIntBetween(100, 200);
   return {
-    pCenter,
-    radius,
+    pCenterMultipliers: {
+      x: gaussianRng(),
+      y: gaussianRng(),
+    },
+    radiusDivisor: getRandomBetween(0.8, 5),
     color: col[getRandomIntBetween(0, 1)],
     rotationSpeed: getRandomBetween(0.00001, 0.00003),
-    rotationDirection,
-    moveDistance: space.width / getRandomBetween(50, 100),
+    rotationDirection: [-1, 1][getRandomIntBetween(0, 1)],
+    moveDistanceDivisor: getRandomBetween(50, 100),
     moveSpeed: getRandomBetween(30000, 100000),
-    nLines: N_LINES,
-    lineRotationFactor: rotation,
-    lines: [...Array(N_LINES)].map((x, i) => {
-      const l = Line.fromAngle(pCenter, i * ((2 * Math.PI) / N_LINES), radius);
+    nLines,
+    lineRotationFactor: getRandomBetween(0.2, 0.27),
+    lineThicknesDivisor: 800,
+    lineTransparentizeFactors: [...Array(nLines)].map(
+      () => transparentizeFactor * rng()
+    ),
+  };
+};
+
+const circleParams = [...Array(N_CIRCLES)].map(getRandomPortalParams);
+
+const getPortalFromParams = (params, bound) => {
+  const pCenter = new Pt(
+    bound.width * params.pCenterMultipliers.x,
+    bound.height * params.pCenterMultipliers.y
+  );
+  const radius = bound.width / params.radiusDivisor;
+  return {
+    ...params,
+    pCenter,
+    radius,
+    moveDistance: bound.width / params.moveDistanceDivisor,
+    lineThickness: bound.width / params.lineThicknesDivisor,
+    lines: [...Array(params.nLines)].map((x, i) => {
+      const l = Line.fromAngle(
+        pCenter,
+        i * ((2 * Math.PI) / params.nLines),
+        radius
+      );
       l[0] = l.centroid();
       // l.scale(getRandomBetween(1, 1.1), l[0]);
-      l.rotate2D(rotationDirection * 2 * Math.PI * rotation);
+      l.rotate2D(
+        params.rotationDirection * 2 * Math.PI * params.lineRotationFactor
+      );
       return {
         line: l,
-        trans: transparentizeFactor * rng(),
+        trans: params.lineTransparentizeFactors[i],
       };
     }),
   };
@@ -80,11 +100,12 @@ const getRandomLinesCircle = (space) => {
 
 let circles;
 space.add({
-  start: (bound, space) => {
-    const N_CIRCLES = getRandomIntBetween(3, 6);
-    circles = [...Array(N_CIRCLES)].map(() => getRandomLinesCircle(space));
+  start: (bound) => {
+    circles = circleParams.map((portalParams) =>
+      getPortalFromParams(portalParams, bound)
+    );
   },
-  animate: (time, ftime, space) => {
+  animate: (time) => {
     circles.forEach((c) => {
       const movedPCenter = Line.fromAngle(
         new Pt(0, 0),
@@ -99,10 +120,15 @@ space.add({
         _l.moveBy(movedPCenter);
         _l.rotate2D(c.rotationDirection * -time * c.rotationSpeed, c.pCenter);
         form
-          .strokeOnly(transparentize(c.color, trans), space.width / 800)
+          .strokeOnly(transparentize(c.color, trans), c.lineThickness)
           .line(_l);
       });
     });
+  },
+  resize: (bound) => {
+    circles = circleParams.map((portalParams) =>
+      getPortalFromParams(portalParams, bound)
+    );
   },
 });
 
