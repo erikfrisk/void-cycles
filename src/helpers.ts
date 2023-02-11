c0cd68cb38e357e1e847ab57c3acee37ff6de55e
Erik Frisk <erik@frisk.cc>
2023-02-12 02:31:20 +0800

    Fix getRandomIntBetween

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,10 @@
 export const getRngHelpers = (rng) => ({
   getRandomBetween: (min, max) => min + (max - min) * rng(),
-  getRandomIntBetween: (min, max) => Math.round(min + (max - min) * rng()),
+  getRandomIntBetween: (min, max) => {
+    const _min = Math.ceil(min);
+    const _max = Math.floor(max);
+    return Math.floor(rng() * (_max - _min + 1)) + _min;
+  },
   gaussianRng: (res = 10) => [...Array(res)].reduce((r) => r + rng(), 0) / res,
 });
 
